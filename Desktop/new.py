a23e50134203ce930757af23c1f7637c2e60dd95
NicktheCodebeginner <yejiaming20@126.com>
2017-11-05 14:32:46 +0800

    new trans

--- Desktop/new.py
+++ Desktop/new.py
@@ -1,18 +1,29 @@
 import json
 import sys
+import jieba
+import time
 
 def change(fileName):
     fileContent = open(fileName)
     trainSet = json.load(fileContent)
     length = len(trainSet)
-    new_train = []
+    newTrainSet = []
+    startTime = time.time()
     for i in range(length):
         tempDic = {}
-        tempDic['file_path'] = '/caption_train_images_20170902/' + trainSet[i]['image_id']
-        tempDic['captions'] = trainSet[i]['caption']
-        new_train.append(tempDic)
+        filePath = '/caption_train_images_20170902/' + trainSet[i]['image_id']
+        captions = []
+        for j in range(5):
+            cutSentence = jieba.lcut(trainSet[i]['caption'][j], cut_all = False)
+            captions += cutSentence
+        tempDic['file_path'] = filePath
+        tempDic['captions'] = captions
+        newTrainSet.append(tempDic)
+        print('%dth captions has done' % i)
+    doneTime = time.time()
     newFile = open('newCaptions.json', 'w')
-    newFile.write(json.dumps(new_train))
+    newFile.write(json.dumps(newTrainSet))
+    print("Time consume: %ds" % int(doneTime - startTime))
 
 if __name__ == '__main__':
     f_input = sys.argv[1]
